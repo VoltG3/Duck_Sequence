74d3958d1fc0d86346e9dc4e7765f00036a85a9e
VoltG3 <kristiandanoss@gmail.com>
2025-04-29 12:53:12 +0200

    [ ADDING ] - fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useEffect } from "react"
+import { GlobalStyles } from "./GlobalStyles"
 import SectionHeader from "./sections/section.header"
 import SectionBody from "./sections/section.body"
 import SectionFooter from "./sections/section.footer"
-import {GlobalStyles} from "./GlobalStyles";
 
 function App() {
+    useEffect(() => {
+        fetch('/assets/data.json')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok')
+                } //console.log(response)
+                return response.json()
+            })
+            .then((data) => {
+               console.log("Fetched JSON data: ", data)
+            })
+            .catch((error) => {
+                console.log("Error fetching JSON: ",error)
+            })
+    }, [])
+
   return (
     <div style={{
         display: "flex",
