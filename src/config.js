727de2e71ed617644704be14f77f4f94af6c60e7
VoltG3 <kristiandanoss@gmail.com>
2025-04-29 12:33:22 +0200

    [ ADDING ] - img

---
+++ src/config.js
@@ -0,0 +1,17 @@
+
+const config = {
+    persons : {
+        URL_person_01: process.env.PUBLIC_URL + "/assets/img/person_01.png",
+        URL_person_02: process.env.PUBLIC_URL + "/assets/img/person_02.png",
+        URL_person_03: process.env.PUBLIC_URL + "/assets/img/person_03.png",
+        URL_person_04: process.env.PUBLIC_URL + "/assets/img/person_04.png",
+        URL_person_05: process.env.PUBLIC_URL + "/assets/img/person_05.png",
+        URL_person_06: process.env.PUBLIC_URL + "/assets/img/person_06.png",
+        URL_person_07: process.env.PUBLIC_URL + "/assets/img/person_07.png",
+        URL_person_08: process.env.PUBLIC_URL + "/assets/img/person_08.png",
+        URL_person_09: process.env.PUBLIC_URL + "/assets/img/person_09.png",
+        URL_person_10: process.env.PUBLIC_URL + "/assets/img/person_10.png",
+    }
+}
+
+export default config
